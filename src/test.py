42521ee261a20a602198f4d8979b9d0acab420d5
Marek <marek.pecha.89@seznam.cz>
2024-06-20 14:08:53 +0000

    test.py: add plot

--- src/test.py
+++ src/test.py
@@ -1,12 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot(x: np.ndarray, y: np.ndarray) -> None:
+    plt.plot(x, y)
+    plt.show()
 
 
 def main() -> None:
     np_array1: np.ndarray = np.array([1, 2, 3])
     np_array2: np.ndarray = 2 * np_array1
-    np_array3: np.ndarray = np_array1 + np_array2
 
+    np_array3: np.ndarray = np_array1 + np_array2
     print(np_array3)
+
+    plot(x=np_array2, y=np_array3)
 
 
 if __name__ == '__main__':
