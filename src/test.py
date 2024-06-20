3f0504c3ef826e39030bcefe922957d08c05b5a5
Marek <marek.pecha.89@seznam.cz>
2024-06-20 13:01:19 +0000

    test from container

--- src/test.py
+++ src/test.py
@@ -4,7 +4,9 @@ import numpy as np
 def main() -> None:
     np_array1: np.ndarray = np.array([1, 2, 3])
     np_array2: np.ndarray = 2 * np_array1
-    print(np_array2)
+    np_array3: np.ndarray = np_array1 + np_array2
+
+    print(np_array3)
 
 
 if __name__ == '__main__':
