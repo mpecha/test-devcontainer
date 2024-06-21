3a5f20e125d5bd6ba2854a02251def7ec55336cc
Marek <marek.pecha.89@seznam.cz>
2024-06-21 12:10:53 +0000

    test: add np_array4

--- src/test.py
+++ src/test.py
@@ -12,7 +12,8 @@ def main() -> None:
     np_array2: np.ndarray = 2 * np_array1
 
     np_array3: np.ndarray = np_array1 + np_array2
-    print(np_array3)
+    np_array4: np.ndarray = 4 * np_array3
+    print(np_array4)
 
     plot(x=np_array2, y=np_array3)
 
